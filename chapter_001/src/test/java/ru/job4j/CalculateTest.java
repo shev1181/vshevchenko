a0b6c5958faa133e93cc8e7dcbd1420826b846ea
Valeriy Shevchenko <shev1181@gmail.com>
2018-02-25 22:42:46 -0500

    3.1 Элементарный калькулятор

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Valeriy Shevchenko (parsentev@yandex.ru)
-* @version $Id$
-* @since 15 of February 2018
-*/
+ * Test.
+ *
+ * @author Valeriy Shevchenko (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 15 of February 2018
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Valeriy Shevchenko";
-    String expect = "Echo, echo, echo : Valeriy Shevchenko"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Valeriy Shevchenko";
+        String expect = "Echo, echo, echo : Valeriy Shevchenko";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculatorTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    public void whenSubtractOneMinusOneThenZero () {
+        calc.subtruct(1D, 1D);
+        double result = calc.getResult();
+        double expected = 0D;
+        assertThat(result, is(expected));
+    }
+ 
+   public void whenMultiplyOneAsteriskOneThenOne() {
+        calc.multiply(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+
+    public void whenDivideOneSlashOneThenOne() {
+        calc.divide(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+     
+
+}
