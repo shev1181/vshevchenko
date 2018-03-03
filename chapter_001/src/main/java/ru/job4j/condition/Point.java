a9a309672eb79be90f734a12c2c1eed2a2baefe8
Valeriy Shevchenko <shev1181@gmail.com>
2018-03-02 19:40:19 -0500

    "3.4 Расстояние меду точками в системе координат"

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,27 @@
+﻿package ru.job4j.condition;
+
+/**
+ * @author Valeriy Shevchenko (shev1181@gmail.com)
+ * @version $Id$
+ * @since February 15. 2018
+ */
+
+public double distanceTo(Point that) {
+    return Math.sqrt(
+            Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+    );
+
+public static void main(String[] args) {
+    Point a = new Point(0, 1);
+    Point b = new Point(2, 5);
+    // сделаем вызов метода
+    System.out.println("x1 = " + a.x);
+    System.out.println("y1 = " + a.y);
+    System.out.println("x2 = " + b.x);
+    System.out.println("y2 = " + b.y);
+
+    double result = a.distanceTo(b);
+    System.out.println("Расстояние между точками А и В : " + result);
+}
+
+}
